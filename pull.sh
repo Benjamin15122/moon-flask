d42cdeeb6b9c7519cdb4e276f59af839a09176ef
Aplusplus <njuaplusplus@gmail.com>
2015-04-25 12:46:36 +0800

    Add pages/shengweian

--- pull.sh
+++ pull.sh
@@ -3,4 +3,6 @@
 MOON_DIR=/var/www/moon
 
 git -C $MOON_DIR pull origin master
+git -C $MOON_DIR submodule init
+git -C $MOON_DIR submodule update
 git -C $MOON_DIR submodule foreach git pull origin master
