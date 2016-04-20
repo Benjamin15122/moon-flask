7ea7105828d7eb3f6d04accd069040bbe10de07c
jyy <jiangyy@outlook.com>
2016-04-20 17:39:51 +0800

    add KaTeX support for wiki

--- moon/spar.py
+++ moon/spar.py
@@ -7,6 +7,8 @@ from citation import makeJinjaBlockPattern
 @app.route('/spar/<path:path>', methods = ['GET'])
 def spar(path = '/'):
     try:
+        if path == 'kwiki':
+            path += '/index.html'
         if path.endswith('/'):
             path += 'index.html'
         if path.split('.')[-1] in ['html']: # HTML may be raw file or a markdown
@@ -24,7 +26,11 @@ def spar(path = '/'):
                     content = md.convert(fp.read().decode('utf-8'))
                     meta = md.Meta
                     title = ''.join( meta.get('title', ['']) )
-                return flask.render_template('spar.html', content = content, title = title, nofooter = True)
+                if path.startswith('kwiki'):
+                    template = 'wiki.html'
+                else:
+                    template = 'spar.html'
+                return flask.render_template(template, content = content, title = title, nofooter = True)
         return flask.send_from_directory(SPAR_DIR, path)
     except Exception, e:
         return str(e)
