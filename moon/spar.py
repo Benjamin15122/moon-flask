bc84cc971fb1f6920d17630c18818680cd636c88
jyy <jiangyy@outlook.com>
2016-09-12 17:49:48 +0800

    refactor spar.py

--- moon/spar.py
+++ moon/spar.py
@@ -2,46 +2,50 @@ import os, flask, markdown
 from moon import *
 
 from citation import makeJinjaBlockPattern
-
 from spar_ext import ArithmatexExtension, SparPeople
+
+def render_markdown(path, md_path):
+    kwiki = path.startswith('kwiki/')  # is this page belong to KWiki?
+    index = (path == '/') # is this page the index page?
+
+    with open(md_path, 'r') as fp:
+        extensions = [
+            'markdown.extensions.extra',
+            'markdown.extensions.meta',
+            'markdown.extensions.toc',
+        ]
+
+        if kwiki:
+            extensions += [
+                ArithmatexExtension(),
+            ]
+            
+        raw = fp.read().decode('utf-8')
+        if index: raw += SparPeople() # TODO: replace it with a plugin
+
+        md = markdown.Markdown(extensions = extensions)
+        makeJinjaBlockPattern(md) # render bibtex; TODO: refactor code to have meaningful names
+
+    return flask.render_template(
+        'wiki.html' if kwiki else 'spar.html',  # template
+        content = md.convert(raw), # contents
+        title = ''.join(md.Meta.get('title', [''])), # title
+        nofooter = True
+    )
+
 
 @app.route('/spar/', methods = ['GET'])
 @app.route('/spar/<path:path>', methods = ['GET'])
 def spar(path = '/'):
-    try:
-        if path == 'kwiki':
-            return flask.redirect('/spar/kwiki/')
-        if path.endswith('/'):
-            path += 'index.html'
-        if path.split('.')[-1] in ['html']: # HTML may be raw file or a markdown
-            md_path = SPAR_DIR + os.path.sep + '.'.join(path.split('.')[:-1] + ['md'])
-            if os.path.isfile(md_path):
-                kwiki = path.startswith('kwiki/')
-                template = 'wiki.html' if kwiki else 'spar.html'
+    if path.endswith('/') and os.path.isdir(SPAR_DIR + os.path.sep + path):
+        return render_markdown(path, SPAR_DIR + os.path.sep + path + '/index.md')
 
-                with open(md_path, 'r') as fp:
-                    extensions = ['markdown.extensions.extra',
-                         'markdown.extensions.meta',
-                         'markdown.extensions.toc',]
+    if path.split('.')[-1] in ['html']:
+        # test whether the page should be rendered from a markdown file
+        md_path = SPAR_DIR + os.path.sep + '.'.join(path.split('.')[:-1]) + '.md'
+        if os.path.isfile(md_path):
+            return render_markdown(path, md_path)
 
-                    if kwiki:
-                        extensions.append(ArithmatexExtension())
-                        
-                    raw = fp.read().decode('utf-8')
-                    if path == '/index.html':
-                        raw += SparPeople()
-
-                    md = markdown.Markdown(extensions = extensions)
-
-                    makeJinjaBlockPattern(md)
-
-                    content = md.convert(raw)
-
-                    meta = md.Meta
-                    title = ''.join( meta.get('title', ['']) )
-
-                return flask.render_template(template, content = content, title = title, nofooter = True)
-        return flask.send_from_directory(SPAR_DIR, path)
-    except Exception, e:
-        return str(e)
+    # otherwise sends the raw data
+    return flask.send_from_directory(SPAR_DIR, path)
 
