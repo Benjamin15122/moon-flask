2b283c0c754be0481b267fbc76d0eebab90fefca
Tianxiao Gu <tianxiao.gu@gmail.com>
2016-10-17 16:33:18 +0800

    Make webhook lightweight

--- moon/webhook.py
+++ moon/webhook.py
@@ -11,14 +11,15 @@ def refresh_moon():
     ''' Refresh the code repository
 
     '''
-    ret_code = subprocess.call(GIT_PULL_MOON)
-    print('execute "%s" with ret %d' % (' '.join(GIT_PULL_MOON), ret_code))
+    # Make webhook more lightweight
+    # ret_code = subprocess.call(GIT_PULL_MOON)
+    # print('execute "%s" with ret %d' % (' '.join(GIT_PULL_MOON), ret_code))
 
-    ret_code = subprocess.call(GIT_INIT_SUBMODULES)
-    print('execute "%s" with ret %d' % (' '.join(GIT_INIT_SUBMODULES), ret_code))
+    # ret_code = subprocess.call(GIT_INIT_SUBMODULES)
+    # print('execute "%s" with ret %d' % (' '.join(GIT_INIT_SUBMODULES), ret_code))
 
-    ret_code = subprocess.call(GIT_UPDATE_SUBMODULES)
-    print('execute "%s" with ret %d' % (' '.join(GIT_UPDATE_SUBMODULES), ret_code))
+    # ret_code = subprocess.call(GIT_UPDATE_SUBMODULES)
+    # print('execute "%s" with ret %d' % (' '.join(GIT_UPDATE_SUBMODULES), ret_code))
 
     ret_code = subprocess.call(GIT_PULL_SUBMODULES)
     print('execute "%s" with ret %d' % (' '.join(GIT_PULL_SUBMODULES), ret_code))
