1f53ca8182283e021b11d06b7bdba7747d5cd0e7
jyy <jiangyy@outlook.com>
2018-08-01 10:29:13 +0800

    update scripts

--- pages/people/yyjiang/gen.py
+++ pages/people/yyjiang/gen.py
@@ -1,32 +1,34 @@
 import yaml, bibtexparser
 
 def render(name, id, p, fname):
-    with open(fname, 'w') as fp:
-        print >> fp, 'title: {0}'.format(p.title)
-        print >> fp
-        print >> fp, '## {0}'.format(p.title)
-        print >> fp
-        print >> fp, '> {{{{ render_bib_entry(g.site.spar_paper.{0}, hl="Yanyan Jiang") }}}}'.format(id)
-        if 'desc' in p.__dict__:
-            print >> fp
-            print >> fp, p.desc.encode('utf-8')
+  lines = [
+    f'title: {p.title}\n',
+    f'## {p.title}\n',
+    f'> {{{{ render_bib_entry(g.site.spar_paper.{id}, hl="Yanyan Jiang") }}}}\n',
+  ]
+  if 'desc' in p.__dict__:
+    lines += [f'{p.desc}\n']
+
+  with open(fname, 'w') as fp:
+    fp.write('\n'.join(lines))
 
 parser = bibtexparser.bparser.BibTexParser()
 parser.customization = bibtexparser.customization.convert_to_unicode
 db = {}
 
 class Paper:
-    pass
+  pass
 
 for p in bibtexparser.load(open('../../spar/spar.bib'), parser=parser).entries:
-    paper = Paper()
-    for (k, v) in p.items():
-        setattr(paper, k.lower(), v)
-    db[paper.id] = paper
-    
+  paper = Paper()
+  for (k, v) in p.items():
+    setattr(paper, k.lower(), v)
+  db[paper.id] = paper
+  
 for (name, values) in yaml.load(open('papers.yaml', 'r')).items():
-    id = values['id']
-    for (k, v) in values.items():
-        setattr(db[id], k, v)
-    render(name, id, db[id], 'pubs/{0}.md'.format(name))
-    print(name, )
+  id = values['id']
+  for (k, v) in values.items():
+    setattr(db[id], k, v)
+  paper = db[id]
+  render(name, id, paper, 'pubs/{0}.md'.format(name))
+  print(name, '-', paper.title)
