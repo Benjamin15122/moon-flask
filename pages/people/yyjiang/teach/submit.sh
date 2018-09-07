f1802329bcc819052da7995d9f9ae9fd47c4a7cc
jyy <jiangyy@outlook.com>
2018-09-07 23:57:33 +0800

    update script

--- pages/people/yyjiang/teach/submit.sh
+++ pages/people/yyjiang/teach/submit.sh
@@ -1,13 +1,14 @@
-#!/bin/bash
-
 set -e
 
 [ -z ${STUID} ]   && echo "STUID must be set (RTFM)"   && exit
 [ -z ${STUNAME} ] && echo "STUNAME must be set (RTFM)" && exit
-task=${PWD##*/}
+echo -n "The task to be submitted > "
+read task
+
+cwd=${PWD##*/}
 echo "${STUID} (${STUNAME}) submitting ${task}..."
 
 wiki=$(curl -m 1 -Ls -w '%{url_effective}' -o /dev/null 'moon.nju.edu.cn/~jyywiki' | grep -o 'http://[^/]*/')
 tarball=$(mktemp -q).tar.bz2
-bash -c "cd .. && tar cj ${task} > ${tarball}"
+bash -c "cd .. && tar cj ${cwd} > ${tarball}"
 curl -F "task=${task}" -F "id=${STUID}" -F "name=${STUNAME}" -F "submission=@${tarball}" ${wiki}upload
